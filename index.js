061d9b9932450e0cf024098485ce7bf6ea5c241d
MD Aminul Islam <aminulsc12@gmail.com>
2022-11-25 15:30:09 +0600

    user collection added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectID, ObjectId } = require('bson');
+const {  ObjectId } = require('bson');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
 
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 
@@ -20,6 +20,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const categoryColletion = client.db('usedProductMarket').collection('category');
+        const bookingColletion = client.db('usedProductMarket').collection('booking');
+        const usersColletion = client.db('usedProductMarket').collection('users');
 
 
         app.get('/category', async (req, res) => {
@@ -31,12 +33,32 @@ async function run() {
 
         app.get('/category/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) }
+            const query = {_id: ObjectId(id)}
             const category = await categoryColletion.findOne(query)
             res.send(category)
+        })  
+
+        app.get('/bookings', async(req, res)=>{
+            const email = req.query.email
+            const query = {email: email}
+            const bookings = await bookingColletion.find(query).toArray()
+            res.send(bookings)
+        })
+        
+
+        app.post('/bookings', async(req, res)=>{
+            const booking = req.body;
+            const result = await bookingColletion.insertOne(booking)
+            res.send(result)
         })
 
-        // app.put('/category', async(req, res)=>{
+        app.post('/users', async(req, res)=>{
+            const user = req.body;
+            const result = await usersColletion.insertOne(user)
+            res.send(result)
+        })
+
+        // app.get('/category', async(req, res)=>{
         //     const filter = {};
         //     const options = {upsert: true}
         //     const updatedDoc = {
@@ -47,6 +69,12 @@ async function run() {
         //     const category = await categoryColletion.updateMany(filter, updatedDoc, options)
         //     res.send(category)
         // })
+
+       
+
+
+
+
     }
     finally {
 
